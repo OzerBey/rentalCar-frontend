0634f8c24e4db8da595f0d1bfdbfcf455fc4c817
OzerBey <ysnozer.zer@gmail.com>
2021-03-20 14:17:27 +0300

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrandComponent } from './brand/brand.component';
-import { CarComponent } from './car/car.component';
-import { ColorComponent } from './color/color.component';
-import { CustomerComponent } from './customer/customer.component';
-import { NaviComponent } from './navi/navi.component';
-import { RentalComponent } from './rental/rental.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { CarComponent } from './components/car/car.component';
+import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { RentalComponent } from './components/rental/rental.component';
+
 
 @NgModule({
   declarations: [
