5314f782329f4ebd65e11a84cdc873638a01cc0b
OzerBey <ysnozer.zer@gmail.com>
2021-03-20 19:17:52 +0300

    general service added and updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { CarComponent } from './components/car/car.component';
@@ -8,7 +10,6 @@ import { ColorComponent } from './components/color/color.component';
 import { CustomerComponent } from './components/customer/customer.component';
 import { NaviComponent } from './components/navi/navi.component';
 import { RentalComponent } from './components/rental/rental.component';
-
 
 @NgModule({
   declarations: [
@@ -18,13 +19,10 @@ import { RentalComponent } from './components/rental/rental.component';
     ColorComponent,
     CustomerComponent,
     NaviComponent,
-    RentalComponent
+    RentalComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarResponseModel } from '../models/carResponseModel';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CarService {
+  apiUrl = 'http://localhost:52129/api/cars/getall';
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {}
+
+  getCars():Observable<CarResponseModel>{
+    console.log('transfer is started');
+    return this.httpClient.get<CarResponseModel>(this.apiUrl);
+  }
 }
